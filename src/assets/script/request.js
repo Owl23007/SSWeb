c4e98766eae76d27c27775b167168ddc89e30f14
MoXueYao <moxueyao.love@gmail.com>
2024-08-14 17:18:28 +0800

    add text function

--- src/assets/script/request.js
+++ src/assets/script/request.js
@@ -1,6 +1,26 @@
 import axios from 'axios'
+
+async function request_add_text(text_content, text_title, name) {
+    const res = await axios({
+        url: 'http://localhost:8080/api/addtext',//请求地址
+        method: 'post',//请求方式
+        data: {//请求参数
+            Text_content: text_content,
+            Text_title: text_title,
+            username: name
+        },
+    })
+    return res;
+}
 async function request_text_list() {
-    return await axios.get('http://127.0.0.1:8080/api/text/list/')
+    const res = await axios({
+        url: 'http://localhost:8080/api/gettext',
+        method: 'post',
+        data: {//请求参数
+            Text_time: 123456
+        },
+    })
+    return res;
 }
 
 async function register_request(username, password, email) {
@@ -28,4 +48,4 @@ async function login_request(username, password) {
     return res;
 }
 
-export { request_text_list, register_request, login_request }
+export { request_text_list, request_add_text, register_request, login_request }
--- src/components/Text.vue
+++ src/components/Text.vue
@@ -1,28 +1,47 @@
 <template>
     <div class="text_container">
         <ul>
-            
+            <li v-for="(item, index) in text_list" :key="index">
+                <a>{{item['Text_title']}}</a>
+                <a>{{item['Text_content']}}</a>
+                <a>{{item['username']}}</a>
+            </li>
         </ul>
+    </div>
+    <div>
+        <a>内容</a>
+        <input v-model="text_content" type="text" />
+        <a>作者</a>
+        <input v-model="name" type="text" />
+        <a>标题</a>
+        <input v-model="text_title" type="text" />
+        <button @click="addText">提交</button>
     </div>
 </template>
 
 <script>
 import { onMounted, ref } from "vue"
-import { request_text_list } from "../assets/script/request.js"
+import { request_text_list, request_add_text } from "../assets/script/request.js"
 export default {
     name: 'textPage',
     setup() {
         const text_list = ref([]);
+        const text_content = ref('');
+        const text_title = ref('');
+        const name = ref('');
 
         onMounted(async () => {
             console.log("textPage");
-
-            await request_text_list().then(res => {
-                text_list.value = res.data;
-            });
+            const res = await request_text_list()
+            text_list.value = res['data']['data'];
+            console.log(res['data'])
         });
+        const addText = async () => {
+            const res = await request_add_text(text_content.value, text_title.value, name.value)
+            console.log(res['data']['message'])
+        }
         return {
-            text_list
+            text_list, text_content, text_title, name, addText
         };
     }
 };
